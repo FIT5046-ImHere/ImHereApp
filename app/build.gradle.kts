60d7b4e3ed79d75c9eff8d25dcbfba1161472864
Mahdi <mahdiafkar0@gmail.com>
2025-04-14 21:23:28 +1000

    Say my name change!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
 }
