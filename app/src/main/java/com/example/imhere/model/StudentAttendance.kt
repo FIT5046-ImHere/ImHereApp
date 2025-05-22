1a257e98f49e1d133f29af8109da2f32005db97b
FarizioKautsar <kheruzy@gmail.com>
2025-05-22 12:54:51 +1000

    Added StudentAttendance.ky

---
+++ app/src/main/java/com/example/imhere/model/StudentAttendance.kt
@@ -0,0 +1,7 @@
+package com.example.imhere.model
+
+class StudentAttendance (
+    studentName: String = "",
+    attendanceId: String = "",
+    status: AttendanceStatus = AttendanceStatus.PRESENT
+)
