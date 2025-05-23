e16fb23d4f115e414a48c11a9fa1492da710ea58
jenn01101 <btra0027@student.monash.edu>
2025-05-23 17:25:46 +1000

    Attendance service fix query fixed

--- app/src/main/java/com/example/imhere/model/service/impl/AttendanceServiceImpl.kt
+++ app/src/main/java/com/example/imhere/model/service/impl/AttendanceServiceImpl.kt
@@ -119,7 +119,7 @@ class AttendanceServiceImpl @Inject constructor(
         startDate: Date?,
         endDate: Date?
     ): List<Attendance> {
-        var query: Query = classSessionCollection
+        var query: Query = collection
 
         if (studentId != null) {
             query = query.whereEqualTo("studentId", studentId)
